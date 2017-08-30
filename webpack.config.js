c929857218c67417774ca2e2d8a9b494d6927521
Erica <erica.de.graaf.borkent@gmail.com>
2017-08-30 15:41:52 +0200

    Change build

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require("webpack");
 module.exports = {
    entry: './src/main.js',
   output: {
-    path: 'C:\\Users\\François ANDRE\\workspaceWordpress\\aeris-commons-vue-components\\dist',
+    path: '',
     publicPath: 'dist/',
     filename: 'build.js'
   },
